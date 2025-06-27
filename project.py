62ce07b06a7269a1ad7bf78b70c0011d80e9574a
QuantitativeSneezing <jatsburgern@Jasondesktop.>
2025-06-27 10:01:53 -0400

    cleaned data (more or less)

--- project.py
+++ project.py
@@ -1,7 +1,2 @@
-import torch
+# import torch
 # print(torch.cuda.is_available())
-import pandas as pd
-import os
-meta= "./symbols_valid_meta.csv"
-symbol_meta= pd.read_csv(meta)
-print(symbol_meta.info())
